f494d2297937fed43b52296d9017a49bad5bb982
bernanda0 <inotbnri@gmail.com>
2023-08-26 21:23:26 +0700

    server : creating redis module and polls repository

--- server/src/polls/polls.service.ts
+++ server/src/polls/polls.service.ts
@@ -1,34 +1,65 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, Logger } from '@nestjs/common';
 import { createPollID, createUserID } from 'src/utils/id';
 import {
   CreatePollFields,
   JoinPollFields,
   RejoinPollFields,
 } from './polls.type';
+import { PollsRepository } from './polls.repository';
 
 @Injectable()
 export class PollsService {
+  private readonly logger = new Logger(PollsService.name);
+  constructor(private readonly pollsRepository: PollsRepository) {}
+
   async createPoll(fields: CreatePollFields) {
     const pollID = createPollID();
     const userID = createUserID();
 
-    return {
+    const createdPoll = await this.pollsRepository.createPoll({
       pollID,
       userID,
       ...fields,
+    });
+
+    // TODO - create an access token of pollID and userID
+    return {
+      poll: createdPoll,
+      // access token
     };
   }
 
   async joinPoll(fields: JoinPollFields) {
     const userID = createUserID();
 
+    // this.logger.debug(
+    //   `Fetching poll with ID: ${fields.pollID} for user with ID: ${userID}`,
+    // );
+    // const joinedPoll = await this.pollsRepository.getPoll(fields.pollID);
+
+    this.logger.debug(
+      `Joiining poll with ID: ${fields.pollID} for user with ID: ${userID} with name: ${fields.name}`,
+    );
+
+    const joinedPoll = await this.pollsRepository.addParticipant({
+        pollID: fields.pollID,
+        userID: userID,
+        name: fields.name,
+    });
+
     return {
-      ...fields,
-      userID,
+      poll: joinedPoll,
+      // access token,
     };
   }
 
   async rejoinPoll(fields: RejoinPollFields) {
-    return fields;
+    this.logger.debug(
+      `Rejoining poll with ID: ${fields.pollID} for user with ID: ${fields.userID} with name: ${fields.name}`,
+    );
+
+    const joinedPoll = await this.pollsRepository.addParticipant(fields);
+
+    return joinedPoll;
   }
 }
--- server/src/polls/polls.type.ts
+++ server/src/polls/polls.type.ts
@@ -1,16 +1,44 @@
 export type CreatePollFields = {
+  topic: string;
+  votesPerVoter: number;
+  name: string;
+};
+
+export type JoinPollFields = {
+  pollID: string;
+  name: string;
+};
+
+export type RejoinPollFields = {
+  pollID: string;
+  userID: string;
+  name: string;
+};
+
+// repository types
+export type CreatePollData = {
+  pollID: string;
+  topic: string;
+  votesPerVoter: number;
+  userID: string;
+};
+
+export type AddParticipantData = {
+  pollID: string;
+  userID: string;
+  name: string;
+};
+
+
+// 
+export interface Participants {
+    [participantID: string]: string;
+}
+
+export interface Poll {
+    id: string;
     topic: string;
     votesPerVoter: number;
-    name: string;
-  };
-  
-  export type JoinPollFields = {
-    pollID: string;
-    name: string;
-  };
-  
-  export type RejoinPollFields = {
-    pollID: string;
-    userID: string;
-    name: string;
-  };
+    participants: Participants;
+    adminID: string
+}
